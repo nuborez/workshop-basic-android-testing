036c5584c88ca2b18fd037a915f7f3efcc910c75
nuborez <leevn2011@hotmail.com>
2018-04-17 16:33:08 +0700

    update testcase no_id

--- app/src/test/java/workshop/testing/data/model/RecipeTest.java
+++ app/src/test/java/workshop/testing/data/model/RecipeTest.java
@@ -27,6 +27,7 @@ public class RecipeTest {
         Recipe recipe = Recipe.readFromStream(stream);
 
         assertNotNull(recipe);
+        assertEquals("Water",recipe.title);
         assertEquals("Put glass under tap. Open tap. Close tap. Drink.",recipe.description);
     }
 
