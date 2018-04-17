d8fcaf215f5ea53127764e62e2faa3394c0faf41
nuborez <leevn2011@hotmail.com>
2018-04-17 16:30:21 +0700

    testcase for water

---
+++ app/src/test/java/workshop/testing/data/model/RecipeTest.java
@@ -0,0 +1,26 @@
+package workshop.testing.data.model;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
+public class RecipeTest {
+    @Test
+    public void testWater(){
+        InputStream stream = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
+
+        Recipe recipe = Recipe.readFromStream(stream);
+
+        assertNotNull(recipe);
+        assertEquals("water",recipe.id);
+        assertEquals("Water",recipe.title);
+        assertEquals("Put glass under tap. Open tap. Close tap. Drink.",recipe.description);
+
+    }
+
+
+
+
+}
